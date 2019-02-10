694da37effbcbbc7a5b92f70c2aaadd5ed2a560b
Dmitry Tirkai <dmitry.tirkai@gmail.com>
2019-02-10 17:29:40 +0300

    Оживлен слайдер с партнерами

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,21 @@
 import 'window'
 class App {
     static init() {
+        const carousel = document.querySelector(".js-carousel");
+        tns({
+            container: carousel,
+            items: 3,
+            slideBy: "page",
+            nav: true,
+            touch: true,
+            mouseDrag: true,
+            autoplayButtonOutput: false,
+            controlsText: [
+                `<img src="dist/img/arrow-right.svg" class="invert">`,
+                `<img src="dist/img/arrow-right.svg">`
+            ]
+        });
+        
         console.log('APP INITED');
     }
 }
